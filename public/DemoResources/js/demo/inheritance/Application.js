a989cdf69ca1795be782d9cfcc1d2db40d003498
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-02-10 04:43:24 -0700

    Added TestModule to test custom paths.

--- public/DemoResources/js/demo/inheritance/Application.js
+++ public/DemoResources/js/demo/inheritance/Application.js
@@ -1,7 +1,9 @@
 Build.paths.main = '/Resources/js/';
+Build.paths.demo = '/DemoResources/js/';
 Build(function() {
 	console.log('Application started...');
-	Build.load([ 'build.ui.Module' ], function() {
+	Build.load([ 'build.ui.Module', 'demo::demo.ui.TestModule' ], function() {
 		var module = new build.ui.Module();
+		var testModule = new demo.ui.TestModule();
 	});
 });
---
+++ public/DemoResources/js/demo/ui/TestModule.js
@@ -0,0 +1,7 @@
+Build('demo.ui.TestModule', [], function(define) {
+	define({
+		$constructor : function() {
+			console.log('demo.ui.TestModule');
+		}
+	});
+});
