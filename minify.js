96e9c94de3cbd97becc27f85b3e0d2187aba7615
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-02-10 16:42:41 -0700

    Moved min folder.

--- minify.js
+++ minify.js
@@ -3,7 +3,7 @@ new compressor.minify({
 	type : 'gcc',
 	language : 'ECMASCRIPT5',
 	fileIn : 'public/Resources/js/build/Build.js',
-	fileOut : 'public/Resources/js/min/Build.min.js',
+	fileOut : 'public/Resources/min/js/Build.min.js',
 	callback : function(err, min) {
 		console.log('Minification complete.');
 		// console.log('Error: ' + err);
