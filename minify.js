3ff5723a03070361d3b79764681c296c13bfbcf5
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-02-10 15:31:22 -0700

    Added minification support.

---
+++ minify.js
@@ -0,0 +1,12 @@
+var compressor = require('node-minify');
+new compressor.minify({
+	type : 'gcc',
+	language : 'ECMASCRIPT5',
+	fileIn : 'public/Resources/js/build/Build.js',
+	fileOut : 'public/Resources/js/min/Build.min.js',
+	callback : function(err, min) {
+		console.log('Minification complete.');
+		// console.log('Error: ' + err);
+		// console.log(min);
+	}
+});
