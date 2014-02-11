5f551614174d2cd47e9ac1636c20cdc41933f9dc
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-02-11 12:16:40 -0700

    Moved min folder.

--- config-minify.js
+++ config-minify.js
@@ -2,25 +2,25 @@ module.exports = {
 	files : [ {
 		algorithm : 'gcc',
 		source : [ '/public/Resources/js/build/Build.js' ],
-		destination : 'public/Resources/min/js/Build.min.js',
+		destination : 'public/min/js/Build.min.js',
 		include : [ '.js' ],
 		exclude : []
 	}, {
 		algorithm : 'gcc',
 		source : [ /* '/public/Resources/js/build/Build.js', */'/public/Resources/js' ],
-		destination : 'public/Resources/min/js/Build-browser.min.js',
+		destination : 'public/min/js/Build-browser.min.js',
 		include : [ '.js' ],
 		exclude : []
 	}, {
 		algorithm : 'gcc',
 		source : [ '/public/Resources/js/build/Build.js', '/public/NodeResources/js' ],
-		destination : 'public/Resources/min/js/Build-node.min.js',
+		destination : 'public/min/js/Build-node.min.js',
 		include : [ '.js' ],
 		exclude : []
 	}, {
 		algorithm : 'gcc',
 		source : [ /* '/public/Resources/js/build/Build.js', */'/public/Resources/js', '/public/NodeResources/js' ],
-		destination : 'public/Resources/min/js/Build-all.min.js',
+		destination : 'public/min/js/Build-all.min.js',
 		include : [ '.js' ],
 		exclude : []
 	} ]
