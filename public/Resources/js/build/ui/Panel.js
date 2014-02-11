9b2c5954ba7ba8df598801fb08e26eecb42f8ffd
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-02-11 04:14:58 -0700

    Updated UI element types.

--- public/Resources/js/build/ui/Panel.js
+++ public/Resources/js/build/ui/Panel.js
@@ -1,8 +1,8 @@
 Build('build.ui.Panel', [ 'build.ui.Widget' ], function(define, $super) {
 	define({
 		$extends : 'build.ui.Widget',
-		$constructor : function() {
-			$super(this)();
+		$constructor : function(type) {
+			$super(this)(type);
 			this.children = [];
 		},
 		$prototype : {
--- public/Resources/js/build/ui/form/Form.js
+++ public/Resources/js/build/ui/form/Form.js
@@ -1,8 +1,8 @@
 Build('build.ui.form.Form', [ 'build.ui.Panel' ], function(define, $super) {
 	define({
 		$extends : 'build.ui.Panel',
-		$constructor : function() {
-			$super(this)('form');
+		$constructor : function(type) {
+			$super(this)(type || 'form');
 		}
 	});
 });
