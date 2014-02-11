9afa21ba9adf830c805ebd650c0e9f153ae44cad
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-02-10 23:07:04 -0700

    Created HashRouter.

---
+++ public/Resources/js/build/HashRouter.js
@@ -0,0 +1,37 @@
+Build('build.HashRouter', [], function(define) {
+	define({
+		$constructor : function() {
+			this.routes = {};
+		},
+		$prototype : {
+			handler : function(event) {
+				for ( var route in routes) {
+					if (routes.hasOwnProperty(route)) {
+						var routeDef = routes[route];
+						var params = event.newURL.match(routeDef.regex);
+						if (params) {
+							routeDef.enter.apply(routeDef.enter, params.slice(1));
+						}
+					}
+				}
+			},
+			listen : function() {
+				window.addEventHandler('hashchange', this.handler);
+			},
+			stop : function() {
+				window.removeEventHandler('hashchange', this.handler);
+			},
+			add : function(route, enter, exit) {
+				this.routes[route] = {
+					route : route,
+					enter : enter,
+					exit : exit,
+					regex : new RegExp(route.replace(/\//g, "\\/").replace(/:(\w*)/g, "(\\w*)"))
+				};
+			},
+			remove : function(route) {
+				delete this.routes[route];
+			}
+		}
+	});
+});
