e24aa6eb7474a0b22c3accb4584f29c336479625
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-02-19 15:32:46 -0700

    Updated Panel build.

--- public/Resources/js/build/ui/Panel.js
+++ public/Resources/js/build/ui/Panel.js
@@ -2,21 +2,20 @@ Build('build.ui.Panel', [ 'build::build.ui.Widget' ], function(define, $super) {
 	define({
 		$extends : 'build.ui.Widget',
 		$constructor : function(parameters) {
-			$super(this)(parameters);
 			this.children = ko.observableArray();
+			$super(this)(parameters);
 		},
 		$prototype : {
+			build : function() {
+				ko.applyBindingsToNode(this.element, {
+					foreachElement : this.children
+				});
+			},
 			addChild : function(child) {
 				this.children.push(child);
-				if (!this.template) {
-					this.appendChild(child);
-				}
 			},
 			removeChild : function(child) {
 				this.children.splice(array.indexOf(child), 1);
-				if (!this.template) {
-					this.removeChild(child);
-				}
 			}
 		}
 	});
