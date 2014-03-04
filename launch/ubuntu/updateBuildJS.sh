4871ce28fb5345721463c0f11da03708bccfaf25
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-03-04 12:55:07 -0700

    Updated launch configuration.

--- launch/ubuntu/updateBuildJS.sh
+++ launch/ubuntu/updateBuildJS.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo Update BuildJS
 
-forever stop /etc/nodejs/Buildjs/buildjsapp.js
+forever stop /etc/nodejs/BuildJS/buildjsapp.js
 
 cd /etc/nodejs/BuildJS
 git reset --hard
 git pull origin master
 npm install
 
-forever start /etc/nodejs/Buildjs/buildjsapp.js
+forever start /etc/nodejs/BuildJS/buildjsapp.js
