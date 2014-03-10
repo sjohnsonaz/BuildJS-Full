880cf15c78d7b9ec1f4a4c625b7a125570827c1a
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-03-10 11:55:21 -0700

    Fixed demo usage of AuthenticationWidget.

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ Build.paths.main = config.Build.backend.paths.main;
 Build.paths.demo = config.Build.backend.paths.demo;
 Build.paths.buildnode = config.Build.backend.paths.buildnode;
 Build(function() {
-	Build.load([ 'buildnode::build.mvc.server.DynamicServer', 'buildnode::build.mvc.controller.UserController', 'buildnode::build.mvc.controller.TestController' ], function() {
+	Build.load([ 'buildnode::build.mvc.server.DynamicServer', 'buildnode::build.mvc.controller.AuthenticationController', 'buildnode::build.mvc.controller.UserController', 'buildnode::build.mvc.controller.TestController' ], function() {
 		var server = new build.mvc.server.DynamicServer(config);
-		//Build.load(config.controllers, function () {
-		//	server.addControllers(server.app, config.controllers);
-		//});
+		// Build.load(config.controllers, function () {
+		// server.addControllers(server.app, config.controllers);
+		// });
+		server.addController(new build.mvc.controller.AuthenticationController(server.app));
 		server.addController(new build.mvc.controller.TestController(server.app));
 		server.addController(new build.mvc.controller.UserController(server.app));
 		server.start();
