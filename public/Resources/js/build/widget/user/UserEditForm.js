023fcf0d5698492afe71f64d2fb997cfb5cb5f0b
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-03-19 14:54:45 -0700

    Started building UserEditForm.

--- public/Resources/js/build/widget/user/UserEditForm.js
+++ public/Resources/js/build/widget/user/UserEditForm.js
@@ -1,8 +1,64 @@
-Build('build.widget.user.UserEditForm', [ 'build::build.ui.form.Form' ], function(define, $super, merge, safe) {
+Build('build.widget.user.UserEditForm', [ 'build::build.ui.form.Form', 'build::build.ui.form.Div', 'build::build.ui.form.Text', 'build::build.ui.form.Password', 'build::build.ui.form.Button', 'build::build.ui.form.Submit', 'build::build.ui.form.FormControl',
+		'build::build.ui.form.Label' ], function(define, $super, merge, safe) {
 	define({
 		$extends : 'build.ui.form.Form',
 		$constructor : function() {
 			$super(this)();
+			this.method('POST');
+			this.action('#');
+			this.message = build.ui.form.Div.create();
+			this.username = build.ui.form.Text.create();
+			this.username.placeholder('Username');
+			// this.password = build.ui.form.Password.create();
+			// this.password.placeholder('Password');
+			this.cancel = build.ui.form.Button.create('Cancel');
+			this.submit = build.ui.form.Submit.create('Save');
+			this.authenticationServiceConnection = authenticationServiceConnection;
+
+			this.addChild(this.message);
+			this.addChild(build.ui.form.FormControl.create(build.ui.form.Label.create('Username'), this.username));
+			this.addChild(build.ui.form.FormControl.create(build.ui.form.Label.create('Password'), this.password));
+			this.addChild(this.submit);
+		},
+		$prototype : {
+			build : function() {
+				$super().build(this)();
+				this.addEvent('submit', function(form, event) {
+					event.preventDefault();
+					// Force update for saved passwords.
+					this.username.text(this.username.element.value);
+					this.password.text(this.password.element.value);
+					this.login();
+					return false;
+				}, false, this);
+			},
+			wrap : function(model) {
+				this.username.text(model.username);
+				this.password.text(model.password);
+			},
+			unwrap : function(model) {
+				// model.username = this.username.text();
+				// model.password = this.password.text();
+			},
+			clear : function() {
+				this.message.text('');
+				this.username.text('');
+				this.password.text('');
+			},
+			login : function(success, error) {
+				this.authenticationServiceConnection.login(this.username.text(), this.password.text(), function(data, request) {
+					console.log(data);
+					if (data.success) {
+						this.runCallbacks('loginSuccess', data, request);
+						safe(success)(data, request);
+					} else {
+						this.message.text(data.message);
+						console.log('not logged in');
+					}
+				}.bind(this), function(request) {
+					safe(error)(data, request);
+				}.bind(this));
+			}
 		}
 	});
 });
