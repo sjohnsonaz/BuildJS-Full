fb09fb210417b762d8ea85a631be0078131a1d41
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-03-19 14:48:07 -0700

    Created directAppend variable in Panel. Created button-group mixin in LESS styles.

--- public/Resources/js/build/ui/Panel.js
+++ public/Resources/js/build/ui/Panel.js
@@ -4,17 +4,24 @@ Build('build.ui.Panel', [ 'build::build.ui.Widget' ], function(define, $super) {
 		$constructor : function() {
 			$super(this)();
 			this.children = ko.observableArray();
+			this.directAppend = false;
 		},
 		$prototype : {
 			build : function() {
 				$super().build(this)();
-				var div = document.createElement('div');
-				div.dataset.bind = 'element: $data';
-				div.className = 'panel-iterator';
-				this.element.appendChild(div);
-				ko.applyBindingsToNode(this.element, {
-					foreach : this.children
-				});
+				if (this.directAppend) {
+					ko.applyBindingsToNode(this.element, {
+						foreachElement : this.children
+					});
+				} else {
+					var div = document.createElement('div');
+					div.dataset.bind = 'element: $data';
+					div.className = 'panel-iterator';
+					this.element.appendChild(div);
+					ko.applyBindingsToNode(this.element, {
+						foreach : this.children
+					});
+				}
 			},
 			addChild : function(child) {
 				this.children.push(child);
--- public/Resources/js/build/ui/form/ButtonGroup.js
+++ public/Resources/js/build/ui/form/ButtonGroup.js
@@ -3,6 +3,7 @@ Build('build.ui.form.ButtonGroup', ['build::build.ui.Panel'], function(define, $
 		$extends: 'build.ui.Panel',
 		$constructor: function () {
 			$super(this)();
+			this.directAppend = true;
 		}
 	});
 });
