4a598f81c21c4d56553142be6a13646645189af5
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-03-23 12:58:04 -0700

    Added partial UserEditForm implementation.

--- public/Resources/js/build/widget/user/UserWidget.js
+++ public/Resources/js/build/widget/user/UserWidget.js
@@ -4,18 +4,25 @@ Build('build.widget.user.UserWidget', [ 'build::build.ui.SwitcherPanel', 'build:
 		$extends : 'build.ui.SwitcherPanel',
 		$constructor : function(userServiceConnection) {
 			$super(this)();
+			this.user = null;
 			this.userServiceConnection = userServiceConnection || new build.service.UserServiceConnection();
 			this.userListForm = build.widget.user.UserListForm.create(this.userServiceConnection);
 			this.userListForm.addCallback('viewUser', function(user) {
 				console.log(user);
-			});	
+			});
 			this.userListForm.addCallback('editUser', function(user) {
+				this.user = user;
+				this.active(1);
+				this.userEditForm.model(user);
 				console.log(user);
-			});	
+			}.bind(this));
 			this.userListForm.addCallback('deleteUser', function(user) {
 				console.log(user);
-			});	
+			});
 			this.addChild(this.userListForm);
+
+			this.userEditForm = build.widget.user.UserEditForm.create(this.userServiceConnection);
+			this.addChild(this.userEditForm);
 		},
 		$prototype : {
 			list : function() {
