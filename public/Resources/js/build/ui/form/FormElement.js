3102aad5d28f466945598943bee213af0ed8a273
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-03-24 15:06:23 -0700

    Added observable property 'val' to FormElement.

--- public/Resources/js/build/ui/form/FormElement.js
+++ public/Resources/js/build/ui/form/FormElement.js
@@ -5,7 +5,19 @@ Build('build.ui.form.FormElement', [ 'build::build.ui.Widget' ], function(define
 			$super(this)();
 		},
 		$prototype : {
-
+			build : function() {
+				Object.defineProperty(this.element, 'val', {
+					get : function() {
+						return this.value;
+					},
+					set : function(value) {
+						this.value = value;
+					}
+				});
+				this.element.addEventListener('change', function() {
+					this.val = this.value;
+				});
+			}
 		}
 	});
 });
