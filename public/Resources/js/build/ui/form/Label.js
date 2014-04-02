d6eb90e5b7852e3fd6f222a244cb9263c4051999
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-04-02 12:34:44 -0700

    Fixed Label init method.

--- public/Resources/js/build/ui/form/Label.js
+++ public/Resources/js/build/ui/form/Label.js
@@ -5,15 +5,20 @@ Build('build.ui.form.Label', [ 'build::build.ui.form.FormElement' ], function(de
 			$super(this)(text);
 			this.type = 'label';
 			this.watchValue('control', control);
-			this.watchAttribute('for', 'forId');
-			this.subscribe('control', function(value) {
-				if (this.control) {
-					var control = this.control.element || this.control;
-					this.forId = control.id;
-				} else {
-					this.forId = '';
-				}
-			});
+			this.watchAttribute('forId', 'for');
+		},
+		$prototype : {
+			init : function(text, control) {
+				$super().init(this)(text);
+				this.subscribe('control', function(value) {
+					if (this.control) {
+						var control = this.control.element || this.control;
+						this.forId = control.id;
+					} else {
+						this.forId = '';
+					}
+				}.bind(this));
+			}
 		}
 	});
 });
