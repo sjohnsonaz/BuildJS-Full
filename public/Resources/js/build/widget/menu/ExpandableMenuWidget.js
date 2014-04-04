5b881b883964c450f15ed8120b4dc0f03cf6026e
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-04-04 10:49:56 -0700

    Updated ExpandableMenuWidget expanding.

--- public/Resources/js/build/widget/menu/ExpandableMenuWidget.js
+++ public/Resources/js/build/widget/menu/ExpandableMenuWidget.js
@@ -1,12 +1,20 @@
 Build('build.widget.menu.ExpandableMenuWidget', [ 'build::build.widget.menu.MenuWidget', 'build::build.ui.form.Button' ], function(define, $super, merge, safe) {
 	define({
 		$extends : 'build.widget.menu.MenuWidget',
-		$constructor : function() {
+		$constructor : function ExpandableMenuWidget() {
 			$super(this)();
 			this.expandButton = build.ui.form.Button.create('{{i:bars}}');
 			this.expandButton.addClass('expand-button');
+			this.expandButton.addEvent('click', function(button, event) {
+				this.expand = !this.expand;
+			}.bind(this));
 		},
 		$prototype : {
+			init : function() {
+				$super().init(this)();
+				this.watchClass('expand', 'expandable-menu-widget-expand');
+				this.expand = false;
+			},
 			refreshChildren : function() {
 				$super().refreshChildren(this)();
 				this.element.insertBefore(this.expandButton.element, this.element.firstChild);
