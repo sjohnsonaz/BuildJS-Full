45f12446a8f2caa9230d2d6bb73f2d0c7b04f971
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-04-10 12:36:16 -0700

    Updated SwitcherPanel with HideMode Enum.

--- public/Resources/js/build/ui/SwitcherPanel.js
+++ public/Resources/js/build/ui/SwitcherPanel.js
@@ -7,6 +7,7 @@ Build('build.ui.SwitcherPanel', [ 'build::build.ui.Panel' ], function(define, $s
 			this.subscribe('active', function(value) {
 				this.refreshChildren();
 			}.bind(this));
+			this.watchValue('hideMode', 'DOM');
 		},
 		$prototype : {
 			init : function(active) {
@@ -15,17 +16,32 @@ Build('build.ui.SwitcherPanel', [ 'build::build.ui.Panel' ], function(define, $s
 			refreshChildren : function() {
 				var element = this.element;
 				if (element) {
-					while (element.firstChild) {
-						element.removeChild(element.firstChild);
-					}
-					if (this.children) {
-						var active = this.children[this.active];
-						if (active) {
-							this.childIterator(active, this.active, this.children);
+					switch (this.hideMode) {
+					case 'DOM':
+						while (element.firstChild) {
+							element.removeChild(element.firstChild);
 						}
+						if (this.children) {
+							var active = this.children[this.active];
+							if (active) {
+								this.childIterator(active, this.active, this.children);
+							}
+						}
+						break;
+					case 'DISPLAY':
+						break;
+					case 'VISIBILITY':
+						break;
 					}
 				}
 			},
+		},
+		$static : {
+			HideMode : {
+				'DOM' : 'DOM',
+				'DISPLAY' : 'DISPLAY',
+				'VISIBILITY' : 'VISIBILITY'
+			}
 		}
 	});
 });
