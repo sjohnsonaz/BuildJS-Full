1ed4164da4c9f43c062c679a79ed16c8fbc14b1c
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-04-14 11:53:09 -0700

    Added empty documentation for Panel and Application.

--- public/Resources/js/build/ui/Application.js
+++ public/Resources/js/build/ui/Application.js
@@ -1,12 +1,23 @@
 Build('build.ui.Application', [ 'build::build.ui.Panel', 'build::build.history.HashRouter' ], function(define, $super) {
 	define({
 		$extends : 'build.ui.Panel',
+		/**
+		 * @class build.ui.Application
+		 * @extends build.ui.Panel
+		 * 
+		 * @constructor
+		 * @member router
+		 * @member marginTop
+		 */
 		$constructor : function Application() {
 			$super(this)();
 			this.router = new build.history.HashRouter();
 			this.watchStyle('marginTop', 'marginTop', 'px');
 		},
 		$prototype : {
+			/**
+			 * @method run
+			 */
 			run : function(parent) {
 				parent = parent || parent.element;
 				while (parent.firstChild) {
--- public/Resources/js/build/ui/Panel.js
+++ public/Resources/js/build/ui/Panel.js
@@ -1,6 +1,12 @@
 Build('build.ui.Panel', [ 'build::build.ui.Widget' ], function(define, $super) {
 	define({
 		$extends : 'build.ui.Widget',
+		/**
+		 * @class build.ui.Panel
+		 * @extends build.ui.Widget
+		 * 
+		 * @constructor
+		 */
 		$constructor : function Panel() {
 			$super(this)();
 		}
