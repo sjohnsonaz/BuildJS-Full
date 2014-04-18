3660e183afce8e91969de7a21481b659bfafec6b
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-04-17 18:31:29 -0700

    Added YouTubePanel to DemoApplication.

--- public/DemoResources/js/demo/application/DemoApplication.js
+++ public/DemoResources/js/demo/application/DemoApplication.js
@@ -4,7 +4,7 @@ Build.paths.demo = '/DemoResources/js/';
 
 Build('demo.application.DemoApplication', [ 'build::build.ui.application.AdminApplication', 'build::build.widget.menu.ExpandableMenuWidget', 'build::build.widget.menu.MenuElement', 'build::build.widget.menu.MenuTitle', 'build::build.ui.element.Div',
 		'build::build.ui.tab.TabContainer', 'build::build.ui.tab.TabPanel', 'build::build.widget.authentication.AuthenticationWidget', 'demo::demo.ui.form.WidgetForm', 'demo::demo.ui.form.TestForm', 'demo::demo.ui.form.RemotePanel',
-		'build::build.service.AuthenticationServiceConnection', 'demo::demo.service.TestServiceConnection' ], function(define, $super, helper) {
+		'demo::demo.ui.form.YouTubePanel', 'build::build.service.AuthenticationServiceConnection', 'demo::demo.service.TestServiceConnection' ], function(define, $super, helper) {
 	define({
 		$extends : 'build.ui.application.AdminApplication',
 		$constructor : function Application() {
@@ -27,9 +27,11 @@ Build('demo.application.DemoApplication', [ 'build::build.ui.application.AdminAp
 			var tabPanel0 = build.ui.tab.TabPanel.create('Widget Form');
 			var tabPanel1 = build.ui.tab.TabPanel.create('Template Form');
 			var tabPanel2 = build.ui.tab.TabPanel.create('Remote Panel');
+			var tabPanel3 = build.ui.tab.TabPanel.create('YouTube Panel');
 			tabContainer.addChild(tabPanel0);
 			tabContainer.addChild(tabPanel1);
 			tabContainer.addChild(tabPanel2);
+			tabContainer.addChild(tabPanel3);
 			this.homePanel.addChild(tabContainer);
 
 			// Add form and button
@@ -46,6 +48,9 @@ Build('demo.application.DemoApplication', [ 'build::build.ui.application.AdminAp
 
 			var remotePanel = demo.ui.form.RemotePanel.create();
 			tabPanel2.addChild(remotePanel);
+
+			var youTubePanel = demo.ui.form.YouTubePanel.create();
+			tabPanel3.addChild(youTubePanel);
 		},
 		$prototype : {
 			init : function() {
---
+++ public/DemoResources/js/demo/ui/form/YouTubePanel.js
@@ -0,0 +1,15 @@
+Build('demo.ui.form.YouTubePanel', [ 'build::build.ui.Panel', 'build::build.widget.video.YouTubeVideo' ], function(define, $super, helper) {
+	define({
+		$extends : 'build.ui.Panel',
+		$constructor : function() {
+			$super(this)();
+			this.youTube = build.widget.video.YouTubeVideo.create('CF0KIsQR6A4');
+			this.addChild(this.youTube);
+		},
+		$prototype : {
+			init : function() {
+				$super().init(this)();
+			}
+		}
+	});
+});
