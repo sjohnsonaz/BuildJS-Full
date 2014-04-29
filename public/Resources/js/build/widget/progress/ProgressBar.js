56d0f149e2cb22545f0abd1e3e5acbecd9c7e679
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-04-29 14:15:52 -0700

    Fixed ProgressBar.

--- public/Resources/js/build/widget/progress/ProgressBar.js
+++ public/Resources/js/build/widget/progress/ProgressBar.js
@@ -1,12 +1,19 @@
 Build('build.widget.progress.ProgressBar', [ 'build::build.ui.Widget' ], function(define, $super) {
 	define({
 		$extends : 'build.ui.Widget',
-		$constructor : function(progress) {
+		$constructor : function ProgressBar(progress) {
 			$super(this)();
 			this.progressElement = document.createElement('div');
 			this.watchValue('progress', progress || 0);
+			this.watchValue('showPercentage', true);
 			this.subscribe('progress', function(value) {
+				this.progressElement.innerHTML = this.showPercentage ? value + '%' : '';
+				value = parseFloat(value);
+				value = typeof value === 'number' ? (value >= 0 ? (value <= 100 ? value : 100) : 0) : 0;
 				this.progressElement.style.width = value + '%';
+			}.bind(this));
+			this.subscribe('showPercentage', function(value) {
+				this.progressElement.innerHTML = value ? this.progress + '%' : '';
 			}.bind(this));
 		},
 		$prototype : {
