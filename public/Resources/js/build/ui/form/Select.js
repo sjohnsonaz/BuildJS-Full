053cad24666e4aa49bf139943d31026d4f7e2499
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-04-30 10:13:43 -0700

    Created empty Select box implementation.

---
+++ public/Resources/js/build/ui/form/Select.js
@@ -0,0 +1,8 @@
+Build('build.ui.form.Select', [ 'build::build.ui.form.FormElement' ], function(define, $super) {
+	define({
+		$extends : 'build.ui.form.FormElement',
+		$constructor : function() {
+			$super(this)();
+		}
+	});
+});
