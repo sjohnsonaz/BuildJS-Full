3fd5d59f39a5c8ec449845e93fa115d8bdae4aba
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-04-30 10:18:40 -0700

    Fixed minor issues in Legacy.polyfill.js.

--- public/Resources/polyfill/Legacy.polyfill.js
+++ public/Resources/polyfill/Legacy.polyfill.js
@@ -23,18 +23,18 @@
 	function docHijack(p) {
 		var old = doc[p];
 		doc[p] = function(v) {
-			return addListen(old(v))
-		}
+			return addListen(old(v));
+		};
 	}
 	function addEvent(on, fn, self) {
 		return (self = this).attachEvent('on' + on, function(e) {
-			var e = e || win.event;
+			e = e || win.event;
 			e.preventDefault = e.preventDefault || function() {
-				e.returnValue = false
-			}
+				e.returnValue = false;
+			};
 			e.stopPropagation = e.stopPropagation || function() {
-				e.cancelBubble = true
-			}
+				e.cancelBubble = true;
+			};
 			fn.call(self, e);
 		});
 	}
@@ -52,7 +52,7 @@
 		win.Element.prototype.addEventListener = addEvent; // IE8
 	else { // IE < 8
 		doc.attachEvent('onreadystatechange', function() {
-			addListen(doc.all)
+			addListen(doc.all);
 		}); // Make sure we also init at domReady
 		docHijack('getElementsByTagName');
 		docHijack('getElementById');
@@ -99,7 +99,7 @@ if (!Object.keys) {
 
 			var result = [], prop, i;
 
-			for (prop in obj) {
+			for ( var prop in obj) {
 				if (hasOwnProperty.call(obj, prop)) {
 					result.push(prop);
 				}
