66e3fe1c15b4828cfd34f86d7e93e227fd191827
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-05-05 13:27:37 -0700

    Moved functionality from Element to Content.

--- public/Resources/js/build/ui/Content.js
+++ public/Resources/js/build/ui/Content.js
@@ -8,8 +8,34 @@ Build('build.ui.Content', [ 'build::build.ui.Widget' ], function(define, $super)
 		/**
 		 * @constructor
 		 */
-		$constructor : function Content() {
-			$super(this)();
+		/**
+		 * @property text
+		 * @property textHelpers
+		 */
+		$constructor : function Content(text) {
+			$super(this)(text);
+			this.watchValue('textHelpers', false);
+		},
+		$prototype : {
+			/**
+			 * @method init
+			 * @param text
+			 */
+			init : function(text) {
+				$super().init(this)(text);
+				var templateParser = null;
+				this.watchProperty('text', 'innerHTML', null, function(value) {
+					return templateParser ? templateParser.parse(value, this) : value;
+				}.bind(this));
+				this.subscribe('textHelpers', function(value) {
+					if (value) {
+						templateParser = build.utility.TemplateParser();
+					} else {
+						templateParser = null;
+					}
+				});
+				this.text = text || '';
+			}
 		}
 	});
 });
--- public/Resources/js/build/ui/element/Element.js
+++ public/Resources/js/build/ui/element/Element.js
@@ -8,33 +8,8 @@ Build('build.ui.element.Element', [ 'build::build.ui.Content', 'build::build.uti
 		/**
 		 * @constructor
 		 */
-		/**
-		 * @property textHelpers
-		 */
 		$constructor : function Element(text) {
 			$super(this)(text);
-			this.watchValue('textHelpers', false);
-		},
-		$prototype : {
-			/**
-			 * @method init
-			 * @param text
-			 */
-			init : function(text) {
-				$super().init(this)(text);
-				var templateParser = null;
-				this.watchProperty('text', 'innerHTML', null, function(value) {
-					return templateParser ? templateParser.parse(value, this) : value;
-				}.bind(this));
-				this.subscribe('textHelpers', function(value) {
-					if (value) {
-						templateParser = build.utility.TemplateParser();
-					} else {
-						templateParser = null;
-					}
-				});
-				this.text = text || '';
-			}
 		}
 	});
 });
