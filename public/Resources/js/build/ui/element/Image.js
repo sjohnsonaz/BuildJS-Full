8a0dd3eefd70c3e17375dfa6ae4566f01ce2bc10
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-05-05 14:14:21 -0700

    Updated Image widget.

--- public/Resources/js/build/ui/element/Image.js
+++ public/Resources/js/build/ui/element/Image.js
@@ -5,10 +5,20 @@
 Build('build.ui.element.Image', [ 'build::build.ui.Widget' ], function(define, $super) {
 	define({
 		$extends : 'build.ui.Widget',
-		$constructor : function Image() {
+		$constructor : function Image(src) {
 			$super(this)();
 			this.type = 'img';
 			this.watchProperty('src');
+			this.watchProperty('loaded', 'src');
+		},
+		$prototype : {
+			init : function(src) {
+				$super().init(this)();
+				this.src = src;
+				this.addEvent('load', function(element, event) {
+					this.publish('loaded');
+				}, false, this);
+			}
 		}
 	});
 });
