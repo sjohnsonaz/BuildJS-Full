d13f63fc63688f4df6873a824f51859b7be711e1
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-05-09 17:48:53 -0700

    Updated update.sh to install production.  Updated package.json to include devDependencies property.

--- launch/ubuntu/update.sh
+++ launch/ubuntu/update.sh
@@ -6,7 +6,7 @@ bash /etc/nodejs/BuildJS/launch/ubuntu/stop.sh
 cd /etc/nodejs/BuildJS
 git reset --hard
 git pull origin master
-npm install
-bower install
+npm install --production
+bower install --production
 
 bash /etc/nodejs/BuildJS/launch/ubuntu/start.sh
