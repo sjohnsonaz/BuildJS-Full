b65e8e87a2bf13e163360e3df9e37519935e5b89
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-05-12 15:10:16 -0700

    Added allow-root flag to bower install.

--- launch/ubuntu/update.sh
+++ launch/ubuntu/update.sh
@@ -7,6 +7,6 @@ cd /etc/nodejs/BuildJS
 git reset --hard
 git pull origin master
 npm install --production
-bower install --production
+bower install --production --allow-root
 
 bash /etc/nodejs/BuildJS/launch/ubuntu/start.sh
