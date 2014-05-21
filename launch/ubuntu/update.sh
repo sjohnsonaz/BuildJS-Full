c34619010d8636ac4bc844ec59c6fa4dd10b552f
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-05-21 16:46:27 -0700

    Updated to have automatic base.css compilation.  Updated to use normalize.css.

--- launch/ubuntu/update.sh
+++ launch/ubuntu/update.sh
@@ -8,5 +8,6 @@ git reset --hard
 git pull origin master
 npm install --production
 bower install --production --allow-root
+lessc ../public/Resources/less/base.less ../public/Resources/css/base.css
 
 bash /etc/nodejs/BuildJS/launch/ubuntu/start.sh
