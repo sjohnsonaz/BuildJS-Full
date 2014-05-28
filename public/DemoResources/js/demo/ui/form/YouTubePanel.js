ec6069378b5adbf112660a914531baec96912e78
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-05-28 14:12:37 -0700

    Disabled YouTube on demo code.

--- public/DemoResources/js/demo/ui/form/YouTubePanel.js
+++ public/DemoResources/js/demo/ui/form/YouTubePanel.js
@@ -1,10 +1,10 @@
-Build('demo.ui.form.YouTubePanel', [ 'build::build.ui.Container', 'build::build.widget.video.YouTubeVideo' ], function(define, $super) {
+Build('demo.ui.form.YouTubePanel', [ 'build::build.ui.Container', '#build::build.widget.video.YouTubeVideo' ], function(define, $super) {
 	define({
 		$extends : 'build.ui.Container',
 		$constructor : function YouTubePanel() {
 			$super(this)();
-			this.youTube = build.widget.video.YouTubeVideo.create('CF0KIsQR6A4');
-			this.addChild(this.youTube);
+			//this.youTube = build.widget.video.YouTubeVideo.create('CF0KIsQR6A4');
+			//this.addChild(this.youTube);
 		},
 		$prototype : {
 			init : function() {
