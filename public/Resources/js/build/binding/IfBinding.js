00edb9aa64ce2048597d778b2726dcf1669b374f
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-08-22 02:20:28 -0700

    Added false condition.

--- public/Resources/js/build/binding/IfBinding.js
+++ public/Resources/js/build/binding/IfBinding.js
@@ -16,19 +16,17 @@ Build('build.binding.IfBinding', [ 'build::build.binding.OneWayBinding' ], funct
 		},
 		$prototype : {
 			update : function(destination, source, value, reverse) {
-				
+
 				var index = this.sources.indexOf(source);
 				this.cache[index] = value;
 				if (this.format) {
 					var condition = this.formatString(this.format, this.cache);
-					var wrappedCondition = 'if (' + condition + ') {this.evaluate();}';
+					var wrappedCondition = 'if (' + condition + ') {this.evaluate(true);} else {this.evaluate(false);}';
 					eval(wrappedCondition);
-				}// else {
-					//destination.text = value;
-				//}
+				}
 			},
-			evaluate: function() {
-				console.log('Condition: ' + true);
+			evaluate : function(value) {
+				console.log('Condition: ' + value);
 			}
 		}
 	});
