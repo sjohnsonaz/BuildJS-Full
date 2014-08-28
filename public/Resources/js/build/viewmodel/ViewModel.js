7579390715fd9808704957579f55f280ed432cd4
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-08-27 18:09:47 -0700

    Added property and wrap methods to ViewModel.

--- public/Resources/js/build/viewmodel/ViewModel.js
+++ public/Resources/js/build/viewmodel/ViewModel.js
@@ -8,8 +8,49 @@ Build('build.viewmodel.ViewModel', [ 'build::build.Module' ], function(define, $
 		/**
 		 * @constructor
 		 */
-		$constructor : function ViewModel() {
+		$constructor : function ViewModel(data) {
 			$super(this)();
+			if (data) {
+
+			}
+		},
+		$prototype : {
+			property : function(name, value, type, validation) {
+				switch (typeof type) {
+				case 'string':
+					switch (type) {
+					case "undefined":
+					case "boolean":
+					case "number":
+					case "string":
+					case "symbol":
+					case "function":
+					case "object":
+						// pass through for now
+						break;
+					default:
+						type = Build.definitions[type];
+						if (type) {
+							value = new type(value);
+						}
+						break;
+					}
+					break;
+				case 'function':
+					value = new type(value);
+					break;
+				}
+				this.watchValue(name, value, null, validation);
+			}
+		},
+		$static : {
+			wrap : function(data) {
+				var result = Object.create(this.prototype);
+				result = this.apply(result, arguments) || result;
+				result.createElement();
+				result.init.apply(result, arguments);
+				return result;
+			},
 		}
 	});
 });
