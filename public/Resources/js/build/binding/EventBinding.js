aff9a1844abb5d37b2be1fa7d93adced288f5d29
Sean Johnson <sjohnson@sjohnsonaz.com>
2014-09-11 01:01:45 -0700

    Added EventBinding.

---
+++ public/Resources/js/build/binding/EventBinding.js
@@ -0,0 +1,23 @@
+/**
+ * @class build.binding.EventBinding
+ * @extends build.binding.BindingHandler
+ */
+Build('build.binding.EventBinding', [ 'build::build.binding.BindingHandler' ], function(define, $super) {
+	define({
+		$extends : 'build.binding.BindingHandler',
+		/**
+		 * @constructor
+		 */
+		$constructor : function EventBinding(destination, source, sourceProperty, destinationProperty) {
+			$super(this)(destination);
+			this.source = source;
+			this.sourceProperty = sourceProperty;
+			this.destinationProperty = destinationProperty;
+		},
+		$prototype : {
+			link : function(destination, source, sourceProperty, destinationProperty) {
+				destination.addEvent(destinationProperty, source[sourceProperty], undefined, source);
+			}
+		}
+	});
+});
