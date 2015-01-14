0ae70df7cf1a3fd4ecadb8fcdca97abe1e3a7f8f
Sean Johnson <sjohnson@sjohnsonaz.com>
2015-01-14 15:31:04 -0700

    Updated minification.  Updated prod.html.

--- config-minify.js
+++ config-minify.js
@@ -8,25 +8,25 @@ var timestamp = null;//min.getTimestamp()
 module.exports = function(min) {
 	return {
 		files : [ {
-			algorithm : 'gcc',
+			algorithm : 'uglifyjs',
 			source : [ '/public/Resources/js/build/Build.js' ],
 			destination : min.getDestination(minFolders.js, 'Build', timestamp, null, 'js'),
 			include : [ '.js' ],
 			exclude : []
 		}, {
-			algorithm : 'gcc',
+			algorithm : 'uglifyjs',
 			source : [ /* '/public/Resources/js/build/Build.js', */'/public/Resources/js' ],
 			destination : min.getDestination(minFolders.js, 'Build', timestamp, 'browser', 'js'),
 			include : [ '.js' ],
 			exclude : []
 		}, {
-			algorithm : 'gcc',
+			algorithm : 'uglifyjs',
 			source : [ '/public/Resources/js/build/Build.js', '/public/NodeResources/js' ],
 			destination : min.getDestination(minFolders.js, 'Build', timestamp, 'node', 'js'),
 			include : [ '.js' ],
 			exclude : []
 		}, {
-			algorithm : 'gcc',
+			algorithm : 'uglifyjs',
 			source : [ /* '/public/Resources/js/build/Build.js', */'/public/Resources/js', '/public/NodeResources/js' ],
 			destination : min.getDestination(minFolders.js, 'Build', timestamp, 'all', 'js'),
 			include : [ '.js' ],
