53bd596414c82173276805c2ff0ae44f40912489
Sean Johnson <sjohnson@sjohnsonaz.com>
2015-02-16 15:47:02 -0700

    Updated Carousel to inherit from Switcher.

--- public/Resources/js/build/widget/carousel/Carousel.js
+++ public/Resources/js/build/widget/carousel/Carousel.js
@@ -1,10 +1,10 @@
 /**
  * @class build.widget.carousel.Carousel
- * @extends build.ui.Container
+ * @extends build.ui.Switcher
  */
-Build('build.widget.carousel.Carousel', [ 'build::build.ui.Container' ], function(define, $super) {
+Build('build.widget.carousel.Carousel', [ 'build::build.ui.Switcher' ], function(define, $super) {
 	define({
-		$extends : 'build.ui.Container',
+		$extends : 'build.ui.Switcher',
 		/**
 		 * @constructor
 		 */
@@ -12,28 +12,34 @@ Build('build.widget.carousel.Carousel', [ 'build::build.ui.Container' ], functio
 			$super(this)();
 			this.container = document.createElement('div');
 			this.container.className = 'carousel-container';
-			this.container.style.left = '0px';
+			//this.container.style.left = '0px';
 			this.innerElement = this.container;
 			this.element.appendChild(this.container);
-			this.element.style.height = '0px';
-			this.watchValue('active', undefined, undefined, function(value, current, cancel) {
-				if (typeof value === 'undefined') {
-					value = 0;
-				}
-				var length = this.children.length;
-				if (length) {
-					value = ((value % length) + length) % length;
-					var left = 0;
-					for (var index = 0; index < value; index++) {
-						left += this.children[index].element.scrollWidth;
+			//this.element.style.height = '0px';
+		},
+		$prototype : {
+			toggleChildElement : function(element, toggle) {
+				if (toggle) {
+					var children = Array.prototype.slice.call(this.innerElement.children);
+					var length = children.length;
+					var position = children.indexOf(element);
+					if (length) {
+						//value = ((value % length) + length) % length;
+						var left = 0;
+						for (var index = 0; index < position; index++) {
+							left += children[index].scrollWidth;
+						}
+						this.container.style.left = -left + 'px';
+						this.element.style.height = element.scrollHeight + 'px';
 					}
-					this.container.style.left = -left + 'px';
-					this.element.style.height = this.children[value].element.scrollHeight + 'px';
-				} else {
-					value = 0;
 				}
-				return value;
-			}.bind(this));
+			},
+			initializeChild : function(child) {
+				return child;
+			},
+			cleanupChild : function(child) {
+				return child;
+			}
 		}
 	});
 });
