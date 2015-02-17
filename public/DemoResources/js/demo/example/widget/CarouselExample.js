510bb74ae9c25961436146dbcb2e1e7b0d2d08b9
Sean Johnson <sjohnson@sjohnsonaz.com>
2015-02-17 14:11:02 -0700

    Added CarouselExample content.

--- public/DemoResources/js/demo/example/widget/CarouselExample.js
+++ public/DemoResources/js/demo/example/widget/CarouselExample.js
@@ -2,7 +2,7 @@
  * @class demo.example.widget.CarouselExample
  * @extends build.ui.Container
  */
-Build('demo.example.widget.CarouselExample', [ 'build::build.ui.Container', 'build::build.ui.element.Header2', 'build::build.ui.element.Paragraph', 'build::build.widget.carousel.Carousel' , 'build::build.ui.Content'], function($define, $super) {
+Build('demo.example.widget.CarouselExample', [ 'build::build.ui.Container', 'build::build.ui.element.Header2', 'build::build.ui.element.Paragraph', 'build::build.widget.carousel.Carousel', 'build::build.ui.Content' ], function($define, $super) {
 	$define({
 		$extends : 'build.ui.Container',
 		$constructor : function MediaExample() {
@@ -11,11 +11,22 @@ Build('demo.example.widget.CarouselExample', [ 'build::build.ui.Container', 'bui
 			this.addChild(header0);
 
 			carousel = build.widget.carousel.Carousel.create();
-			var slide0 = build.ui.Content.create('Slide 0');
+			var slide0 = build.ui.Content
+					.create('\
+					<h1>BuildJS is Awesome!</h1>\
+					<p>Combining class definition and inheritance, module loading, and front-end widgets,\
+					BuildJS lets you develop your Single Page Application in style!</p>\
+					');
 			carousel.addChild(slide0);
-			var slide1 = build.ui.Content.create('Slide 1');
+			var slide1 = build.ui.Content.create('\
+					<h1>Class Definitions</h1>\
+					<p></p>\
+					');
 			carousel.addChild(slide1);
-			var slide2 = build.ui.Content.create('Slide 2');
+			var slide2 = build.ui.Content.create('\
+					<h1>Widgeting Framework</h1>\
+					<p></p>\
+					');
 			carousel.addChild(slide2);
 
 			this.addChild(carousel);
