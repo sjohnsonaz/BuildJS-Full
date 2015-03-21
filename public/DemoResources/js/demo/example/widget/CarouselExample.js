cb0f8836d1d3cc66d5f923bcbf602a6a8c41b8f5
Sean Johnson <sjohnson@sjohnsonaz.com>
2015-03-20 18:29:01 -0700

    Changed CarouselExample to not use interval.

--- public/DemoResources/js/demo/example/widget/CarouselExample.js
+++ public/DemoResources/js/demo/example/widget/CarouselExample.js
@@ -28,7 +28,7 @@ Build('demo.example.widget.CarouselExample', [ 'build::build.ui.Container', 'bui
 					<p></p>\
 					');
 			carousel.carouselSlider.addChild(slide2);
-			carousel.interval = 5000;
+			carousel.interval = 0;
 
 			this.addChild(carousel);
 		}
